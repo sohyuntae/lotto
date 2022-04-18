f80e564cc26abaffec680eb1f7181b5c76f5cc64
IZEX_WEB <http://192.168.100.91:3000/WEB/izexWTM>
2022-04-18 16:23:31 +0900

    Author : 현태 Date : 2022-04-18 Customer : Description : [공통] 1. graphql 기본 적용.

--- frontend/src/components/views/Lotto/lotto_sunni.vue
+++ frontend/src/components/views/Lotto/lotto_sunni.vue
@@ -1,16 +1,18 @@
 <template>
   <div>
     <p> {{ message }} </p>
+    <span v-for="n in 45" v-bind:key="n" > {{ n }}</span>
+    추가 : <input id="test1" v-on:keydown.enter="testCheckEvnt">
   </div>
 </template>
 
 <script type="text/javascript">
-import { createHelpers } from 'vuex-map-fields'
+// import { createHelpers } from 'vuex-map-fields'
 
-const { mapFields } = createHelpers({
-  getterType: 'lotto_sunni/getField',
-  mutationType: 'lotto_sunni/updateField'
-})
+// const { mapFields } = createHelpers({
+//   getterType: 'lotto_sunni/getField',
+//   mutationType: 'lotto_sunni/updateField'
+// })
 
 export default {
   components: {
@@ -18,18 +20,22 @@ export default {
   name: 'lotto_sunni',
   data () {
     return {
-      message: 'test'
+      message: 'test',
+      maxNum: 45,
+      lotteList: []
     }
   },
   computed: {
-    ...mapFields([
-      'lottoNumberList'
-    ])
+    // ...mapFields([
+    //   'lottoNumberList'
+    // ])
   },
   methods: {
     initSys () {
       console.log(1)
-      console.log(this.lottoNumberList)
+    },
+    testCheckEvnt () {
+      console.log(2)
     }
   },
   /* 페이지가 뜰 때 실행 */
---
+++ src/main/java/com/example/testweb/controller/GraphQLController.java
@@ -0,0 +1,82 @@
+package com.example.testweb.controller;
+
+import graphql.*;
+import graphql.language.SourceLocation;
+import io.leangen.graphql.spqr.spring.web.dto.GraphQLRequest;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+public class GraphQLController {
+
+    private final GraphQL graphQL;
+
+    @PostMapping
+    @ResponseBody
+    public Object graphql(@RequestBody GraphQLRequest graphQLRequest, HttpServletRequest raw) throws IOException {
+        ExecutionInput executionInput = ExecutionInput.newExecutionInput()
+                .query(graphQLRequest.getQuery())
+                .context(raw)
+                .build();
+
+        log.info(graphQLRequest.getQuery());
+
+        ExecutionResult executionResult = graphQL.execute(executionInput);
+
+        if (!executionResult.getErrors().isEmpty())
+        {
+            return sanitize(executionResult);
+        }
+
+        return executionResult.toSpecification();
+    }
+
+    private ExecutionResult sanitize(ExecutionResult executionResult)
+    {
+        return new ExecutionResultImpl(executionResult.getErrors().stream()
+            .peek(err -> log.error(err.getMessage()))
+            .map(this::sanitize)
+            .collect(Collectors.toList()));
+    }
+
+    private GraphQLError sanitize(GraphQLError error)
+    {
+        if (error instanceof ExceptionWhileDataFetching)
+        {
+            return new GraphQLError()
+            {
+                @Override
+                public String getMessage()
+                {
+                    Throwable cause = ((ExceptionWhileDataFetching) error).getException().getCause();
+                    return cause instanceof InvocationTargetException ? ((InvocationTargetException) cause).getTargetException().getMessage() : cause.getMessage();
+                }
+
+                @Override
+                public List<SourceLocation> getLocations()
+                {
+                    return error.getLocations();
+                }
+
+                @Override
+                public ErrorType getErrorType()
+                {
+                    return (ErrorType) error.getErrorType();
+                }
+            };
+        }
+        return error;
+    }
+}
