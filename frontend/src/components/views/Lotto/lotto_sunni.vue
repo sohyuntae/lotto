1c4e128f5f4c1efe192c2d6b6b001047b1264b31
sueun <tndms1129@naver.com>
2022-04-19 17:47:45 +0900

    html 구조 변경 css 추가

--- frontend/src/components/views/Lotto/lotto_sunni.vue
+++ frontend/src/components/views/Lotto/lotto_sunni.vue
@@ -1,9 +1,23 @@
 <template>
-  <div>
-    <p> {{ message }} </p>
-    <span v-for="n in 45" v-bind:key="n" > {{ n }}</span>
-    추가 : <input id="test1" v-on:keydown.enter="testCheckEvnt">
-  </div>
+  <section>
+    <article>
+      <h2> {{ message }} </h2>
+    </article>
+
+    <article>
+      <span>추가 : </span>
+      <input type="number" id="test1" v-on:keydown.enter="testCheckEvnt">
+    </article>
+
+    <article>
+      <div class="ball-wrap">
+        <button v-for="n in 45" v-bind:key="n" class="ball">
+          <span> {{ n }}</span>
+        </button>
+      </div>
+    </article>
+
+  </section>
 </template>
 
 <script type="text/javascript">
@@ -36,6 +50,15 @@ export default {
     },
     testCheckEvnt () {
       console.log(2)
+    },
+    randomClass () {
+      const balls = document.getElementsByClassName('ball')
+      setTimeout(() => {
+        for (let i = 0; i < balls.length; i += 1) {
+          const randomNm = `ball${Math.floor(Math.random() * 5) + 1}`
+          balls[i].classList.add(randomNm)
+        }
+      }, 100)
     }
   },
   /* 페이지가 뜰 때 실행 */
@@ -44,6 +67,7 @@ export default {
   /* 페이지가 다 뜨고 난 후 실행 */
   mounted () {
     this.initSys()
+    this.randomClass()
   }
 }
 </script>
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import lotto from '../components/views/Lotto/lotto_sunni'
+import '@/assets/css/base.css'
+import '@/assets/css/index.css'
 
 Vue.use(Router)
 
